3a780728304196f37b694f5bc08933bafb2751fd
wtepfenhart <wtepfenhart@Neptune-II>
2018-09-25 03:11:51 -0400

    Added jUnit test directory for TextInput

--- TextInput/src/TextInput.java
+++ TextInput/src/TextInput.java
@@ -2,6 +2,10 @@
  * 
  */
 
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * @author wtepfenhart
  *
@@ -14,13 +18,26 @@ public class TextInput {
 	public TextInput() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	/**
+	 * the following is for testing junit testing (can you say recursion?)
+	 */
+	public void printHello() {
+		System.out.println("Hello Cruel World");
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TextInput t = new TextInput();
+		try {
+			System.out.println(t.getClass().getSimpleName() + " " + InetAddress.getLocalHost().getHostName());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
