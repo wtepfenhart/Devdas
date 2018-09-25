a4ee341761e797b0c42e077d91387f8c24524775
wtepfenhart <wtepfenhart@Neptune-II>
2018-09-24 21:39:09 -0400

    Added 'Hello World' agents

---
+++ TextInput/src/TextInput.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author wtepfenhart
+ *
+ */
+public class TextInput {
+
+	/**
+	 * 
+	 */
+	public TextInput() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TextOutput/src/TextOutput.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * Author: wtepfenhart
+ * File: TextOutput.java
+ * Date: Sep 24, 2018
+ * Copyright (C) 2018
+ *
+ */
+
+/**
+ * @author wtepfenhart
+ *
+ */
+public class TextOutput {
+
+	/**
+	 * 
+	 */
+	public TextOutput() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
