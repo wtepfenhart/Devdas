d8bd0a295fbfc5db1e9b8e0bbdd1fe114a647ca3
wtepfenhart <wtepfenhart@Neptune-II>
2018-09-24 21:10:49 -0400

    Once again.

---
+++ Manager/src/Manager.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author wtepfenhart
+ *
+ */
+public class Manager {
+
+	/**
+	 * 
+	 */
+	public Manager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
