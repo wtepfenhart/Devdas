b872a493cade4076820c393c3ec70254f846336f
Nicholas-Jason Roache <njrr325@gmail.com>
2018-11-04 11:09:59 -0500

    Update TextInput.java
    removed unnecessary code and took first steps to differentiate ContextFreeText and ContextReliantText

--- TextInput/src/TextInput.java
+++ TextInput/src/TextInput.java
@@ -15,11 +15,7 @@ import commandservice.AgentReaction;
 import commandservice.AgentMessage;
 public class TextInput extends DevdasCore  {
 
-	//Main variables
-	static String kind;
-	static String value;
-	static String input;
-	static String interpreter;
+	
 	Scanner scanner;
 	private static Configuration config;
 	//constants
@@ -28,24 +24,13 @@ public class TextInput extends DevdasCore  {
 	/**
 	 * Has random variables to make testing easier
 	 */
-	public TextInput() {
+	public TextInput( Configuration config) {
 		// TODO Auto-generated constructor stub
 		super(config);
 		scanner = new Scanner(System.in);
-		kind = "kind";
-		value = "value";
-		input = "input";
-		interpreter = "interpret";
+		
 	}
 
-	public TextInput(String kind, String value, String input, String interpreter)
-	{
-		super(config);
-		kind = this.kind;
-		value = this.value;
-		input = this.input;
-		interpreter = this.interpreter;
-	}
 	
 	/**
 	 * the following is for testing junit testing (can you say recursion?)
@@ -54,153 +39,79 @@ public class TextInput extends DevdasCore  {
 		System.out.println("Hello Cruel World");
 	}
 
-	//Initial setters and getters
-	public void setKind(String kind)
-	{
-		this.kind = kind;
-	}
-
-	public String getKind()
-	{
-		return kind;
-	}
-
-	public void setValue(String value)
-	{
-		this.value = value;
-	}
-
-	public String getValue()
-	{
-		return value;
-	}
-
-	public void setInput(String input)
-	{
-		this.input = input;
-	}
-
-	public String getInput()
-	{
-		return input;
-	}
-
-	public void setInterpreter(String interpreter)
-	{
-		this.interpreter = interpreter;
-	}
-
-	public String getInterpreter()
-	{
-		return interpreter;
-	}
 	
-	//Agent Reactions classes
-	public class Kind implements AgentReaction
-	{
-		//Default constructor
-		public Kind()
-		{
-			
-		}
-		public void execute(AgentMessage message)
-		{
-			//Print some kind of message
-		}
-	}
-	public class Value implements AgentReaction
-	{
-		//Default constructor
-		public Value()
-		{
-			
-		}
-		public void execute(AgentMessage message)
-		{
-			//Print some kind of message
-		}
-	}
-	public class Interpreter implements AgentReaction
-	{
-		//Default constructor
-		public Interpreter()
-		{
-			
-		}
-		public void execute(AgentMessage message)
-		{
-			//Print some kind of message
-		}
-	}
-	public class Input implements AgentReaction
-	{
-		//Default constructor
-		public Input()
-		{
-			
-		}
-		public void execute(AgentMessage message)
-		{
-			//Print some kind of message
-		}
-	}
+	
+	
 	/**
 	 * @param args
 	 */
 	//abstract methods
 	public void agentActivity()
 	{
-		try
+		System.out.println("Enter input:");
+		String msg = scanner.nextLine();
+		AgentMessage a = new AgentMessage();
+		if(msg.contains("?") == true)
 		{
-			Thread.sleep(10);
+			a.addParam("Subject", msg);
+			a.setTopic("ContextReliantText");
+			sendAgentMessage(a.getRoute(),a);
 		}
-		catch(InterruptedException e)
-		{
-			e.printStackTrace();
-		}
+		a.addParam("Subject",msg);
+		a.setTopic("Statement");
+		sendAgentMessage(a.getRoute(),a);
 	}
 	
 	@Override
-	
-	 
-	 
 	public void initializeAgentReactions() {
 		// TODO Auto-generated method stub
-		agentInterests.add("Kind");
-		agentReactions.put("Kind",new Kind());
-		agentInterests.add("Value");
-		agentReactions.put("Value", new Value());
-		agentInterests.add("Input");
-		agentReactions.put("Input", new Input());
-		agentInterests.add("Interpreter");
-		agentReactions.put("Interpreter", new Interpreter());
+		agentInterests.add("Statement");
+		agentInterests.add("ContextReliantText");
+	
+
 	}
+	
+	 public class Statement implements AgentReaction
+	 {
+		public Statement()
+		{
+			
+		}
+		public void execute(AgentMessage cmd)
+		{
+			System.out.println(cmd.getParam("Subject"));
+		}
+	 }
+	public class Question implements AgentReaction
+	{
+		public Question()
+		{
+			
+		}
+		public void execute(AgentMessage cmd)
+		{
+			
+		}
+	}
+	 
+	
 
 	
 
 	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
+		Configuration c = new Configuration(args);
+		TextInput t = new TextInput(c);
 		
-		TextInput t = new TextInput();
-		System.out.println("Enter the kind:");
-		String k = t.scanner.next();
-		t.setKind(k);
-		System.out.println("Enter the value:");
-		String v = t.scanner.next();
-		t.setValue(v);
-		System.out.println("Enter the input:");
-		String i = t.scanner.next();
-		t.setInput(i);
-		System.out.println("Enter interpreter:");
-		String it = t.scanner.next();
-		t.setInterpreter(it);
 		try {
 			System.out.println(t.getClass().getSimpleName() + " " + InetAddress.getLocalHost().getHostName());
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 
 	
 }
+
