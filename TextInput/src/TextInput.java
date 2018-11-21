bba55fe48dd0e84466319d9160f9d41100eca296
Nicholas-Jason Roache <njrr325@gmail.com>
2018-11-21 09:49:24 -0500

    Update TextInput.java

--- TextInput/src/TextInput.java
+++ TextInput/src/TextInput.java
@@ -58,48 +58,25 @@ public class TextInput extends DevdasCore  {
 		
 			System.out.println("Enter input:");
 			String msg = scanner.nextLine();
-			AgentMessage a;
-			if(msg.contains("?") == true)
+			AgentMessage a = new AgentMessage();
+			
+			if(a.getDestination() != "")
 			{
-				//Message a is to be sent to the TextOupt Agent
-				a = new AgentMessage();
+				//Agent Message is sent to designated agent
 				a.addParam("Subject", msg);
 				a.setTopic("ContextReliantText");
-				TextOutput o;
-				String d1 = o.hostID;
-				a.setDestination(d1);
 				sendAgentMessage(a.getRoute(),a);
-				//Message b is to sent to the Jokes Agent
+				//Agent Message is sent to text output (default agent)
 				AgentMessage b = new AgentMessage();
 				b.addParam("Subject", msg);
 				b.setTopic("ContextReliantText");
-				//Placeholder for jokes agent
-				String d2 = j.hostID;
-				b.setDestination(d2);
 				sendAgentMessage(b.getRoute(),b);
-				processAgentMessage(b);
-				processAgentMessage(a);
-				
 			}
 			else
 			{
-				a = new AgentMessage();
-				a.addParam("Subject",msg);
-				a.setTopic("Statement");
+				a.addParam("Subject", msg);
+				a.setTopic("ContextFreeText");
 				sendAgentMessage(a.getRoute(),a);
-				processAgentMessage(a);
-				
-			}
-			if(j.isRunning()== true)
-			{
-				try
-				{
-					Thread.sleep(5000);
-				}
-				catch(InterruptedException e)
-				{
-					e.printStackTrace();
-				}
 			}
 		}
 		
@@ -130,5 +107,4 @@ public class TextInput extends DevdasCore  {
 
 	
 }
-
 
